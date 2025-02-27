37fafd43d5cc4fd6ce19a9f7c3551a40473b7046
Vinutha23 <119056807+Vinutha23@users.noreply.github.com>
2025-02-27 10:13:23 +0530

    Delete Day12 directory

--- Day12/TestingDemo/src/main/java/com/mindsprint/project/dao/CustomerDAO.java
+++
@@ -1,8 +0,0 @@
-package com.mindsprint.project.dao;
-import java.util.List;public interface CustomerDAO {
-    public Customer addNewCustomer(Customer customer);
-    public Customer getCustomer(int id);
-    public List<Customer> getAllCustomer();
-    public boolean deleteCustomer(int id);
-    public boolean updateCustomer(int id,String address);
-}
